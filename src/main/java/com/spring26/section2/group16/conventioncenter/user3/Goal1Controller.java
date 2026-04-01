c0681c9a77175bcd908e2292d18b3fe1f1a7534f
nhLabit <2420808@iub.edu.bd>
2026-04-01 12:32:09 +0600

    user-3 goal1,2 scene builder design

--- src/main/java/com/spring26/section2/group16/conventioncenter/user3/Goal1Controller.java
+++ src/main/java/com/spring26/section2/group16/conventioncenter/user3/Goal1Controller.java
@@ -3,5 +3,17 @@ package com.spring26.section2.group16.conventioncenter.user3;
 public class Goal1Controller
 {
     @javafx.fxml.FXML
+    private TextField usernameTF;
+    @javafx.fxml.FXML
+    private PasswordField passwordTF;
+    @javafx.fxml.FXML
+    private Label labelL;
+
+    @javafx.fxml.FXML
     public void initialize() {
-    }}
+    }
+
+    @javafx.fxml.FXML
+    public void loginOnClick(ActionEvent actionEvent) {
+    }
+}
--- src/main/java/com/spring26/section2/group16/conventioncenter/user3/Goal2Controller.java
+++ src/main/java/com/spring26/section2/group16/conventioncenter/user3/Goal2Controller.java
@@ -3,5 +3,19 @@ package com.spring26.section2.group16.conventioncenter.user3;
 public class Goal2Controller
 {
     @javafx.fxml.FXML
+    private TextField nameTF;
+    @javafx.fxml.FXML
+    private ComboBox roleCB;
+    @javafx.fxml.FXML
+    private TextField emailTF;
+    @javafx.fxml.FXML
+    private Label labelL;
+
+    @javafx.fxml.FXML
     public void initialize() {
-    }}
+    }
+
+    @javafx.fxml.FXML
+    public void saveOnClick(ActionEvent actionEvent) {
+    }
+}
