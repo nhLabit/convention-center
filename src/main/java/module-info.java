e0ca8b73a5bf4472b4368961e4faf6e1c89e11cd
nhLabit <2420808@iub.edu.bd>
2026-04-08 02:39:18 +0600

    Dashboard design for user3,4

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,7 +2,8 @@ module com.spring26.section2.group16.conventioncenter {
     requires javafx.controls;
     requires javafx.fxml;
 
-
     opens com.spring26.section2.group16.conventioncenter to javafx.fxml;
+    opens com.spring26.section2.group16.conventioncenter.user3 to javafx.fxml;
+
     exports com.spring26.section2.group16.conventioncenter;
 }
