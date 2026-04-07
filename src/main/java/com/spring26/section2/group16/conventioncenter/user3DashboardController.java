9b0b832ed4d8325493ca995b0eca0fa253930bea
nhLabit <2420808@iub.edu.bd>
2026-04-08 00:45:12 +0600

    Dashboard design for user3,4

---
+++ src/main/java/com/spring26/section2/group16/conventioncenter/user3DashboardController.java
@@ -0,0 +1,44 @@
+package com.spring26.section2.group16.conventioncenter;
+
+import javafx.event.ActionEvent;
+
+public class user3DashboardController
+{
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+    @javafx.fxml.FXML
+    public void rolesOnClick(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void logOutOnClick(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void userListOnClick(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void hallInfoOnClick(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void newUserOnClick(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void bookingReportOnClick(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void staffListOnClick(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void loginOnClick(ActionEvent actionEvent) {
+    }
+
+
+}
---
+++ src/main/java/com/spring26/section2/group16/conventioncenter/user4DashboardController.java
@@ -0,0 +1,42 @@
+package com.spring26.section2.group16.conventioncenter;
+
+import javafx.event.ActionEvent;
+
+public class user4DashboardController
+{
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+    @javafx.fxml.FXML
+    public void logoutOnClick(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void viewHallsOnClick(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void bookHallOnClick(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void registerAccountOnClick(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void cancelBookingOnClick(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void bookingHistoryOnClick(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void makePaymentOnClick(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void loginOnClick(ActionEvent actionEvent) {
+    }
+}
