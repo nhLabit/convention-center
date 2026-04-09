2e4a388b8ed9f555303d9b8537fcaee1fbf88f96
Tanzim27 <tanzimkanij27@gmail.com>
2026-04-09 23:37:09 +0600

    goal3 scene

--- src/main/java/com/spring26/section2/group16/conventioncenter/user5/goal3Controller.java
+++ src/main/java/com/spring26/section2/group16/conventioncenter/user5/goal3Controller.java
@@ -1,7 +1,28 @@
 package com.spring26.section2.group16.conventioncenter.user5;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TextField;
+
 public class goal3Controller
 {
     @javafx.fxml.FXML
+    private TextField EventNameTextField;
+    @javafx.fxml.FXML
+    private ComboBox HallComboBox;
+    @javafx.fxml.FXML
+    private DatePicker EventDatedatepicker;
+
+    @javafx.fxml.FXML
     public void initialize() {
-    }}
+    }
+
+    @javafx.fxml.FXML
+    public void UpdatebuttonOnClick(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void BackButtonOnClick(ActionEvent actionEvent) {
+    }
+}
