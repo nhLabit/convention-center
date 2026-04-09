1bc54249a387388c13398e91d502effc6ceb980f
Tanzim27 <tanzimkanij27@gmail.com>
2026-04-10 04:20:02 +0600

    goals 7 & 8 scene design and edit

--- src/main/java/com/spring26/section2/group16/conventioncenter/user5/goal7Controller.java
+++ src/main/java/com/spring26/section2/group16/conventioncenter/user5/goal7Controller.java
@@ -1,7 +1,32 @@
 package com.spring26.section2.group16.conventioncenter.user5;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+
 public class goal7Controller
 {
     @javafx.fxml.FXML
+    private TableView EventTableView;
+    @javafx.fxml.FXML
+    private TextField EnterNameTextfield;
+    @javafx.fxml.FXML
+    private TableColumn EventNameCol;
+    @javafx.fxml.FXML
+    private TableColumn DateCol;
+    @javafx.fxml.FXML
+    private TableColumn HallCol;
+
+    @javafx.fxml.FXML
     public void initialize() {
-    }}
+    }
+
+    @javafx.fxml.FXML
+    public void SearchOnClick(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void DashboardOnClick(ActionEvent actionEvent) {
+    }
+}
