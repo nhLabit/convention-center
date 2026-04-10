130a0d72f15a6a8e52da564ec6068dcf6bada571
Tanzim27 <tanzimkanij27@gmail.com>
2026-04-11 01:40:55 +0600

    scene switching user5

--- src/main/java/com/spring26/section2/group16/conventioncenter/user5/goal4Controller.java
+++ src/main/java/com/spring26/section2/group16/conventioncenter/user5/goal4Controller.java
@@ -1,9 +1,16 @@
 package com.spring26.section2.group16.conventioncenter.user5;
 
+import com.spring26.section2.group16.conventioncenter.HelloApplication;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class goal4Controller
 {
@@ -23,7 +30,12 @@ public class goal4Controller
     }
 
     @javafx.fxml.FXML
-    public void DashboardOnclick(ActionEvent actionEvent) {
+    public void DashboardOnclick(ActionEvent actionEvent) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("dashboardofUsers/user5Dashboard.fxml"));
+        Scene dashboardScene = new Scene(fxmlLoader.load());
+        Stage currentStage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+        currentStage.setScene(dashboardScene);
+        currentStage.show();
     }
 
     @javafx.fxml.FXML
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,7 +3,13 @@ module com.spring26.section2.group16.conventioncenter {
     requires javafx.fxml;
     //requires com.spring26.section2.group16.conventioncenter;
     requires javafx.graphics;
-    requires com.spring26.section2.group16.conventioncenter;
+    //requires com.spring26.section2.group16.conventioncenter;
+    //requires com.spring26.section2.group16.conventioncenter;
+    //requires com.spring26.section2.group16.conventioncenter;
+    //requires com.spring26.section2.group16.conventioncenter;
+    //requires com.spring26.section2.group16.conventioncenter;
+    //requires com.spring26.section2.group16.conventioncenter;
+    //requires com.spring26.section2.group16.conventioncenter;
 
     opens com.spring26.section2.group16.conventioncenter to javafx.fxml;
     opens com.spring26.section2.group16.conventioncenter.user3 to javafx.fxml;
