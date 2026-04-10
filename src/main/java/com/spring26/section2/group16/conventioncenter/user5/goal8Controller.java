8f8eee0b62a5c055e483ed3ca402606675d11f47
Tanzim27 <tanzimkanij27@gmail.com>
2026-04-10 12:37:57 +0600

    goals 8 scene design edit

--- src/main/java/com/spring26/section2/group16/conventioncenter/user5/goal8Controller.java
+++ src/main/java/com/spring26/section2/group16/conventioncenter/user5/goal8Controller.java
@@ -1,7 +1,31 @@
 package com.spring26.section2.group16.conventioncenter.user5;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+
 public class goal8Controller
 {
     @javafx.fxml.FXML
+    private TableView EventTableView;
+    @javafx.fxml.FXML
+    private ComboBox SelectHallCombox;
+    @javafx.fxml.FXML
+    private Label outputShowLabel;
+    @javafx.fxml.FXML
+    private TableColumn EventCol;
+
+    @javafx.fxml.FXML
     public void initialize() {
-    }}
+    }
+
+    @javafx.fxml.FXML
+    public void SelectOnClick(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void DashboardOnClcik(ActionEvent actionEvent) {
+    }
+}
