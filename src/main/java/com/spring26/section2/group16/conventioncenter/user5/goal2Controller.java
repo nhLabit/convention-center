8a10d325293223de333ad06f7e4f7d835a3c47e5
Tanzim27 <tanzimkanij27@gmail.com>
2026-04-10 23:58:25 +0600

    ss code

--- src/main/java/com/spring26/section2/group16/conventioncenter/user5/goal2Controller.java
+++ src/main/java/com/spring26/section2/group16/conventioncenter/user5/goal2Controller.java
@@ -1,9 +1,16 @@
 package com.spring26.section2.group16.conventioncenter.user5;
 
+import com.spring26.section2.group16.conventioncenter.HelloApplication;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Scene;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class goal2Controller
 {
@@ -31,6 +38,11 @@ public class goal2Controller
     }
 
     @javafx.fxml.FXML
-    public void DashboardOnClick(ActionEvent actionEvent) {
+    public void DashboardOnClick(ActionEvent actionEvent) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("dashboardofUsers/user5Dashboard.fxml"));
+        Scene dashboardScene = new Scene(fxmlLoader.load());
+        Stage currentStage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+        currentStage.setScene(dashboardScene);
+        currentStage.show();
     }
 }
--- src/main/java/com/spring26/section2/group16/conventioncenter/user5DashboardController.java
+++ src/main/java/com/spring26/section2/group16/conventioncenter/user5DashboardController.java
@@ -1,6 +1,12 @@
 package com.spring26.section2.group16.conventioncenter;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class user5DashboardController
 {
@@ -9,38 +15,83 @@ public class user5DashboardController
     }
 
     @javafx.fxml.FXML
-    public void UpdateEventOnClick(ActionEvent actionEvent) {
+    public void UpdateEventOnClick(ActionEvent actionEvent) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("user5/goal3.fxml"));
+        Scene dashboardScene = new Scene(fxmlLoader.load());
+        Stage currentStage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+        currentStage.setScene(dashboardScene);
+        currentStage.show();
     }
 
     @javafx.fxml.FXML
-    public void HomeOnClick(ActionEvent actionEvent) {
+    public void HomeOnClick(ActionEvent actionEvent) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("ProjectLoginPage.fxml"));
+        Scene dashboardScene = new Scene(fxmlLoader.load());
+        Stage currentStage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+        currentStage.setScene(dashboardScene);
+        currentStage.show();
     }
 
     @javafx.fxml.FXML
-    public void createEventOnClick(ActionEvent actionEvent) {
+    public void createEventOnClick(ActionEvent actionEvent) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("user5/goal1.fxml"));
+        Scene dashboardScene = new Scene(fxmlLoader.load());
+        Stage currentStage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+        currentStage.setScene(dashboardScene);
+        currentStage.show();
     }
 
     @javafx.fxml.FXML
-    public void SearchEventOnClick(ActionEvent actionEvent) {
+    public void SearchEventOnClick(ActionEvent actionEvent) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("user5/goal7.fxml"));
+        Scene dashboardScene = new Scene(fxmlLoader.load());
+        Stage currentStage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+        currentStage.setScene(dashboardScene);
+        currentStage.show();
     }
 
     @javafx.fxml.FXML
-    public void DeleteEventOnClick(ActionEvent actionEvent) {
+    public void DeleteEventOnClick(ActionEvent actionEvent) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("user5/goal4.fxml"));
+        Scene dashboardScene = new Scene(fxmlLoader.load());
+        Stage currentStage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+        currentStage.setScene(dashboardScene);
+        currentStage.show();
     }
 
     @javafx.fxml.FXML
-    public void ViewHallAvailabilityOnClick(ActionEvent actionEvent) {
+    public void ViewHallAvailabilityOnClick(ActionEvent actionEvent) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("user5/goal5.fxml"));
+        Scene dashboardScene = new Scene(fxmlLoader.load());
+        Stage currentStage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+        currentStage.setScene(dashboardScene);
+        currentStage.show();
     }
 
     @javafx.fxml.FXML
-    public void ViewEventDetailsOnClick(ActionEvent actionEvent) {
+    public void ViewEventDetailsOnClick(ActionEvent actionEvent) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("user5/goal8.fxml"));
+        Scene dashboardScene = new Scene(fxmlLoader.load());
+        Stage currentStage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+        currentStage.setScene(dashboardScene);
+        currentStage.show();
     }
 
     @javafx.fxml.FXML
-    public void ViewEventsOnClick(ActionEvent actionEvent) {
+    public void ViewEventsOnClick(ActionEvent actionEvent) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("user5/goal2.fxml"));
+        Scene dashboardScene = new Scene(fxmlLoader.load());
+        Stage currentStage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+        currentStage.setScene(dashboardScene);
+        currentStage.show();
     }
 
     @javafx.fxml.FXML
-    public void AssignStafftoEventOnClick(ActionEvent actionEvent) {
+    public void AssignStafftoEventOnClick(ActionEvent actionEvent) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("user5/goal6.fxml"));
+        Scene dashboardScene = new Scene(fxmlLoader.load());
+        Stage currentStage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+        currentStage.setScene(dashboardScene);
+        currentStage.show();
     }
 }
