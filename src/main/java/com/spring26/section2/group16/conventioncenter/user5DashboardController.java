d27eb25f054ef8b0f0067b9e4cd551664d155804
Tanzim27 <tanzimkanij27@gmail.com>
2026-04-10 18:29:17 +0600

    user 5 & 6 dashboard design

---
+++ src/main/java/com/spring26/section2/group16/conventioncenter/user5DashboardController.java
@@ -0,0 +1,46 @@
+package com.spring26.section2.group16.conventioncenter;
+
+import javafx.event.ActionEvent;
+
+public class user5DashboardController
+{
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+    @javafx.fxml.FXML
+    public void UpdateEventOnClick(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void HomeOnClick(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void createEventOnClick(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void SearchEventOnClick(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void DeleteEventOnClick(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void ViewHallAvailabilityOnClick(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void ViewEventDetailsOnClick(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void ViewEventsOnClick(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void AssignStafftoEventOnClick(ActionEvent actionEvent) {
+    }
+}
---
+++ src/main/java/com/spring26/section2/group16/conventioncenter/user6DashboardController.java
@@ -0,0 +1,46 @@
+package com.spring26.section2.group16.conventioncenter;
+
+import javafx.event.ActionEvent;
+
+public class user6DashboardController
+{
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+    @javafx.fxml.FXML
+    public void HomeOnClick(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void DeletePaymentOnClick(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void GenerateInvoiceOnClick(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void SearchPaymentOnClick(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void ViewInvoiceHistoryOnClick(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void GeneratePaymentReportOnClick(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void AddPaymentRecordOnClick(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void UpdatePaymentOnClick(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void ViewPaymentsOnClick(ActionEvent actionEvent) {
+    }
+}
