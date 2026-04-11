637831b0e75c37ba4471c8966d4da63662c6749d
nhLabit <2420808@iub.edu.bd>
2026-04-11 22:03:23 +0600

    user 3 updated code

--- src/main/java/com/spring26/section2/group16/conventioncenter/user4/Goal4Controller.java
+++ src/main/java/com/spring26/section2/group16/conventioncenter/user4/Goal4Controller.java
@@ -1,6 +1,8 @@
 package com.spring26.section2.group16.conventioncenter.user4;
 
 import com.spring26.section2.group16.conventioncenter.HelloApplication;
+import com.spring26.section2.group16.conventioncenter.NonUser.Database;
+import com.spring26.section2.group16.conventioncenter.NonUser.U4G4;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
@@ -12,6 +14,8 @@ import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Goal4Controller
 {
@@ -26,12 +30,31 @@ public class Goal4Controller
     @javafx.fxml.FXML
     private ComboBox<String> hallCB;
 
+    private List<U4G4> databaseArray5 = new ArrayList<>();
+
     @javafx.fxml.FXML
     public void initialize() {
+        hallCB.getItems().addAll("Hall 1", "Hall 2", "Hall 3");
+
     }
 
     @javafx.fxml.FXML
     public void bookingOnClick(ActionEvent actionEvent) {
+        if (hallCB.getValue().isEmpty() || bookingID.getText().isEmpty() || dateDP.getValue() == null ||
+        guestTF.getText().isEmpty()){
+            labelL.setText("Field can't be empty");
+            return;
+        }
+//        for (Database i : databaseArray5 ){
+//            if (i.getBookingID().equals(bookingID.getText())){
+//                labelL.setText("provide unique number");
+//                return;
+
+
+
+//int numberOfGuest, int bookingID, String hallName, LocalDate bookingDate)
+
+
     }
 
     @javafx.fxml.FXML
