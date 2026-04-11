6774905677c67f1e02d348cb848a4a94c8b0fb09
Tanzim27 <tanzimkanij27@gmail.com>
2026-04-12 00:20:29 +0600

    create none user U5G2

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -12,6 +12,7 @@ module com.spring26.section2.group16.conventioncenter {
     //requires com.spring26.section2.group16.conventioncenter;
     //requires com.spring26.section2.group16.conventioncenter;
     //requires com.spring26.section2.group16.conventioncenter;
+    //requires com.spring26.section2.group16.conventioncenter;
     opens com.spring26.section2.group16.conventioncenter.user6 to javafx.fxml;
     opens com.spring26.section2.group16.conventioncenter to javafx.fxml;
     opens com.spring26.section2.group16.conventioncenter.user3 to javafx.fxml;
